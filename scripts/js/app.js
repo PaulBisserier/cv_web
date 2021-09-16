68e8aff27026df20008f1887d3d7cad00526a8e1
paulbis <tfydRuB2QCh33yH>
2021-09-16 11:01:45 +0200

    modif js

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -2,6 +2,6 @@ const cards = document.querySelectorAll(".card__single");
  
 function flipCard() {
   this.classList.toggle("flip");
-  console.log("flip!"); 
+  console.log("N'hésitez pas à me contacter :)"); 
 }
 cards.forEach((card) => card.addEventListener("click", flipCard));
